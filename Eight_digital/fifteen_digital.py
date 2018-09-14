46f2aa56f716ee25e9aad23b43c939c777b5b9fe
AKAGB <374118168@qq.com>
2018-09-14 12:20:03 +0800

    fifV1.2

--- Eight_digital/fifteen_digital.py
+++ Eight_digital/fifteen_digital.py
@@ -9,7 +9,9 @@ class State:
     """
     状态类：记录每一个结点的状态
     """
-    def __init__(self, depth=0, order=[], num=0):
+    aim = [(x, y) for x in range(4) for y in range(4)]
+
+    def __init__(self, depth=0, order=(), num=0):
         self.order = order[:]           # 记录块顺序，下标是块位置，元素是块类型，注意和UI中的order相反
         self.depth = depth              # 当前深度，即走到当前状态所需要的代价
         self.hn = 0                     # 到目标状态的最小估计值
@@ -31,8 +33,7 @@ class State:
         for i in range(16):
             if order[i]:
                 cur = (order[i] - 1) // 4, (order[i] - 1) % 4
-                aim = i // 4, i % 4
-                result += abs(cur[0]-aim[0]) + abs(cur[1]-aim[1])
+                result += abs(cur[0]-State.aim[i][0]) + abs(cur[1]-State.aim[i][1])
         self.hn = result
         self.fn = self.depth + self.hn
 
@@ -101,7 +102,7 @@ if __name__ == '__main__':
     # problem = [1, 2, 3, 4, 5, 7, 0, 8, 9, 6, 10, 12, 13, 14, 11, 15]
     problem = (9, 6, 3, 4, 15, 1, 7, 8, 14, 0, 11, 10, 2, 5, 13, 12)
     # 48:problem = [6, 10, 3, 15, 14, 8, 7, 11, 5, 1, 0, 2, 13, 12, 9, 4]
-    # 49:problem = [14, 10, 6, 0, 4, 9, 1, 8, 2, 3, 5, 11, 12, 13, 7, 15]
+    # 49:problem = (14, 10, 6, 0, 4, 9, 1, 8, 2, 3, 5, 11, 12, 13, 7, 15)
     # 62:problem = [0, 5, 15, 14, 7, 9, 6, 13, 1, 2, 12, 10, 8, 11, 4, 3]
     root = State(0, problem, 0)
     solution = ida_star(root)
