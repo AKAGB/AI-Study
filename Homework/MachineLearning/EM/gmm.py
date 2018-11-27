cfaa37eab524f5654e73cf38ebd15aadc2d2bec8
AKAGB <374118168@qq.com>
2018-11-27 21:14:08 +0800

    EM 2.0

--- Homework/MachineLearning/EM/gmm.py
+++ Homework/MachineLearning/EM/gmm.py
@@ -5,10 +5,12 @@ from math import e
 from math import pi
 from math import log
 from math import inf
+from math import isinf
 
 class GMM:
-    def __init__(self, DataSet, K, limit=10000, ep=1e-10):
-        self.DataSet = np.mat(DataSet)
+    def __init__(self, Data, K, limit=10000, ep=1e-10):
+        self.name = Data.iloc[:, 0]
+        self.DataSet = np.mat(Data.iloc[:,1:])
         self.K = K
         self.N, self.D = np.shape(self.DataSet)
         self.gammas = np.zeros([self.N, self.K])
@@ -26,15 +28,19 @@ class GMM:
             weight /= (np.linalg.det(sigma))**0.5
             x_ = np.transpose(x - miu)
             s_ = np.linalg.inv(sigma)
-            ex = np.sum(np.dot(np.transpose(x_) , np.dot(s_, x_)))
+            ex = float(np.dot(np.transpose(x_) , np.dot(s_, x_)))
             ex *= -0.5
+            if ex < -700:
+                return e**-700
+            if ex > 700:
+                return e**700
             return weight * e**ex
         return distribution
     
     def initialize(self, selectData):
         for i in range(self.K):
             self.mius[i] = self.DataSet[selectData[i]]
-            self.sigmas[i] = np.eye(self.D) * 50
+            self.sigmas[i] = np.eye(self.D) * 20
             self.pi_[i] = 1 / self.K
 
     def Estep(self):
@@ -52,14 +58,21 @@ class GMM:
         Nk = np.sum(self.gammas, axis=0)
         self.pi_ = np.array([Nk[k] / self.N for k in range(self.K)])
         # Update miu
-        self.mius = np.transpose(np.dot(self.DataSet.T, self.gammas) / Nk)
+        # self.mius = np.transpose(np.dot(self.DataSet.T, self.gammas) / Nk)
         # Update sigams
         for i in range(self.K):
             tmp = self.DataSet - self.mius[i]
             s = np.zeros([self.D, self.D])
+            m = np.zeros(self.D)
             for j in range(self.N):
+                m = m + self.gammas[j, i] * self.DataSet[j]
                 s = s + self.gammas[j, i] * np.dot(np.transpose(tmp[j]), tmp[j])
+            if Nk[i] == 0:
+                Nk[i] = 1e-50
+            self.mius[i] = np.mat(m / Nk[i])
             self.sigmas[i] = s / Nk[i]
+            while np.linalg.det(self.sigmas[i]) <= 0:
+                self.sigmas[i] = self.sigmas[i] + np.eye(self.D) * 0.1
 
     def likelihood(self):
         return np.sum(self.logs)
@@ -77,10 +90,26 @@ class GMM:
             self.Mstep()
             new_lh = self.likelihood()
         else:
-            print('迭代达到上限！')
+            print('Iteration reaches the upper limit!')
+    
+    def cluster(self):
+        result = [[] for i in range(self.K)]
+        for i in range(self.N):
+            result[np.argmax(self.gammas[i])].append(self.name[i])
+        level = np.sum(self.mius, axis=1)
+        level = sorted(enumerate(level), key=lambda x : x[1])
+        level = [x[0] for x in level]
+        labels = []
+        for i in range(self.K):
+            labels.append(result[level[i]])
+        
+        return labels
 
 if __name__ == "__main__":
     df = pd.read_csv('football.csv')
-    data = np.mat(df.iloc[:,1:])
-    model = GMM(data, 3, 1000, 1e-6)
-    model.run([0, 2, 8])
+    model = GMM(df, 3, 1000, 1e-10)
+    model.run([0, 5, 8])
+    res = model.cluster()
+    labels = ['First-class', 'Second-class', 'Third-class']
+    for i in range(3):
+        print(labels[i], ':\t', sorted(res[i]), sep='')
